2024e4f2c1bc97e79d9de6f76301400fc4f86266
TaylorFelix <1023614047@qq.com>
2018-04-19 22:21:22 +0800

    Site updated: 2018-04-19 22:21:14

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -8,8 +8,11 @@ $(document).ready(function () {
 
   NexT.utils.lazyLoadPostsImages();
 
+  NexT.utils.registerESCKeyEvent();
+
   NexT.utils.registerBackToTop();
 
+  // Mobile top menu bar.
   $('.site-nav-toggle button').on('click', function () {
     var $siteNav = $('.site-nav');
     var ON_CLASS_NAME = 'site-nav-on';
@@ -17,13 +20,18 @@ $(document).ready(function () {
     var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
     var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
 
-    $siteNav.stop()[animateAction]('normal', function () {
+    $siteNav.stop()[animateAction]('fast', function () {
       $siteNav[animateCallback](ON_CLASS_NAME);
     });
   });
 
-
+  /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
   CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
+  CONFIG.tabs && NexT.utils.registerTabsTag();
+
   NexT.utils.embeddedVideoTransformer();
   NexT.utils.addActiveClassToMenuItem();
 
@@ -38,7 +46,7 @@ $(document).ready(function () {
   $(document).trigger('motion:before');
 
   // Bootstrap Motion.
-  CONFIG.motion && NexT.motion.integrator.bootstrap();
+  CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
 
   $(document).trigger('bootstrap:after');
 });
